57472791896427215fd41918b10ff0612b7a849a
Stephane_Branly <stephanebranly.pro@gmail.com>
2022-06-05 18:51:51 +0200

    [+]

--- scripts/utils.py
+++ scripts/utils.py
@@ -8,10 +8,37 @@
 #                                                       +++##+++::::::::::::::       +#+    +:+     +#+     +#+             #
 #                                                         ::::::::::::::::::::       +#+    +#+     +#+     +#+             #
 #                                                         ::::::::::::::::::::       #+#    #+#     #+#     #+#    #+#      #
-#      Update: 2022/05/31 18:39:07 by Branly, Tran Quoc   ::::::::::::::::::::        ########      ###      ######## .fr   #
+#      Update: 2022/06/05 16:27:46 by Branly, Tran Quoc   ::::::::::::::::::::        ########      ###      ######## .fr   #
 #                                                                                                                           #
 # ************************************************************************************************************************* #
 
+from scipy.cluster.hierarchy import dendrogram
+import numpy as np
+
+# Taken from https://scikit-learn.org/stable/auto_examples/cluster/plot_agglomerative_dendrogram.html
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack([model.children_, model.distances_,
+                                      counts]).astype(float)
+
+    # Plot the corresponding dendrogram
+    default_kwargs = dict(leaf_font_size=10)
+    default_kwargs.update(kwargs or {})
+
+    dendrogram(linkage_matrix, **default_kwargs)
 
 def numeric_categorical_attributs_split(data):
     """
